c95115a91a304eda4edfe834eef39e10b9c82cab
BookCultivator <122209979+BookCultivator@users.noreply.github.com>
2025-02-22 20:18:10 +0800

    更改search_weather

--- Server_base/test.py
+++ Server_base/test.py
@@ -1,16 +1,17 @@
 import requests
 
-url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-58ACE3D3-C4F6-4272-862C-DFD7219B46D1&format=JSON"
 
-data = requests.get(url).json() # 將網頁資料GET下來
-localtion = data["records"]["location"] # 取得location資料
-
+# 取得天氣資訊
 def Seach_weather(city):
+    url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-58ACE3D3-C4F6-4272-862C-DFD7219B46D1&format=JSON"
+    data = requests.get(url).json() # 將網頁資料GET下來
+    location = data["records"]["location"] # 取得location資料
     print("城市 : {}".format(city))
-    for i in localtion:
+    for i in location:
         if i['locationName'] == city:
             for j in i['weatherElement']:
                 if j['elementName'] == "Wx":
+                    print("Time : {}".format(j['time'][0]['startTime']))
                     print("天氣狀況 : {}".format(j['time'][0]['parameter']['parameterName']))
             for j in i['weatherElement']:
                 if j['elementName'] == "MaxT":
