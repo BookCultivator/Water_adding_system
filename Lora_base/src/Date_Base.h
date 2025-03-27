892f000362d39d9c78e07b873ebafd884bdd9e29
BookCultivator <122209979+BookCultivator@users.noreply.github.com>
2025-03-28 03:37:45 +0800

    upload lora function

--- Lora_base/src/Date_Base.h
+++ Lora_base/src/Date_Base.h
@@ -1,2 +1,17 @@
+#include <ArduinoJson.h>
+#include <String.h>
 
+struct get_data {
+    int intData;
+    float floatData;
+    String stringData;
+    StaticJsonDocument<256> jsonData;
+};
+
+struct send_data {
+    int intData;
+    float floatData;
+    String stringData;
+    String jsonData;
+};
 
--- Lora_base/src/Lora_function.h
+++ Lora_base/src/Lora_function.h
@@ -1,122 +1,100 @@
 #include <Arduino.h>
-#include <LoRa.h>
-
-#include <String.h>
 #include <ArduinoJson.h>
+#include <String.h>
+#include <LoRa_E220.h>
+#include <Date_Base.h>
 #include <vector>
 
-struct get_data {
-    int intData;
-    float floatData;
-    String stringData;
-    StaticJsonDocument<256> jsonData;
+int *_M0, *_M1, *_AUX, *_RST, *_RTD, *_TXD;
+LoRa_E220 e220_obj(&Serial2,*_AUX, *_M0, *_M1);
+
+class Lora_function {
+    private:
+        byte ADDl;
+        
+    public:
+        void Lora_pinSet(int &M0, int &M1, int &AUX, int &RST, int &RTD, int &TXD);
+        void Lora_Start();
+        void Lora_Restart();
+        void Lora_getConfig();
+        void Lora_setConfig();
+        void printParameters(struct Configuration configuration);
+        void printModuleInformation(struct ModuleInformation moduleInformation);
+        void Lora_sendData(struct send_data &data);
 };
 
-struct send_data {
-    int intData;
-    float floatData;
-    String stringData;
-    String jsonData;
-};
-
-void Lora_init(int BAND, int NSS, int RST, int DIO0, int SF, int SBW, int CR);
-void Lora_send(String message);
-void Lora_SendData(char type, String deviceID, struct send_data *data_in);
-void Lroa_GetData(char type, String deviceID, struct read_data *data_out);
-
-void Lora_init(int BAND, int NSS, int RST, int DIO0, int SF, int SBW, int CR) {
-    LoRa.setPins(NSS, RST, DIO0);  //設定LoRa模組的腳位
-    if (!LoRa.begin(BAND)) {
-        Serial.println("LoRa init failed. Check your connections.");  //如果初始化失敗，請檢查連接
-        while (true);
-    }
-    Serial.println("LoRa init succeeded.");  //初始化成功
-    LoRa.setSpreadingFactor(SF);  //設定擴散因子
-    LoRa.setSignalBandwidth(SBW);  //設定信號頻寬
-    LoRa.setCodingRate4(CR);  //設定編碼率
-    Serial.println("LoRa init parameters set.");
+void Lora_function::Lora_pinSet(int &M0, int &M1, int &AUX, int &RST, int &RTD, int &TXD) {  //設定Lora模組的腳位
+    _M0 = &M0;
+    _M1 = &M1;
+    _AUX = &AUX;  
+    _RST = &RST;
+    _RTD = &RTD;
+    _TXD = &TXD;
 }
 
-void Lora_SendData(char type, String deviceID, send_data *data_in) {
-    if (deviceID == NULL) {
-        deviceID = "ALL";
-    }
-    if (type == 'int') {    //發送數值資料(無ID指定) =>ALL
-        LoRa.beginPacket();
-        LoRa.write((uint8_t *) &data_in->intData, sizeof(data_in->intData));
-        LoRa.endPacket();
-        Serial.println("Date sent.");
-        return;
-    }
-    else if (type == 'float') {    //發送浮點數資料(無ID指定) =>ALL
-        LoRa.beginPacket();
-        LoRa.write((uint8_t *) &data_in->floatData, sizeof(data_in->floatData));
-        LoRa.endPacket();
-        Serial.println("Date sent.");
-        return;
-    }
-    else if (type == 'String') {   //發送字串資料
-        LoRa.beginPacket();
-        LoRa.print(deviceID + ":" + data_in->stringData);
-        LoRa.endPacket();
-        Serial.println("Date sent.");
-        return;
-    }
-    else if (type == 'json') {     //發送json資料
-        LoRa.beginPacket();
-        LoRa.print(deviceID + ":" + data_in->jsonData);
-        LoRa.endPacket();
-        Serial.println("Date sent.");
-        return;
-    }
-    else {
-        Serial.println("Type error.");  //類型錯誤
-    }
+void Lora_function::Lora_Start() {
+    Serial2.begin(9600, SERIAL_8N1, *_TXD, *_RTD);
+    Serial.println("Serial2 begin.");
+    e220_obj.begin();
 }
 
-void Lroa_GetData(char type, String deviceID, get_data *data_out) {
-    if (type == 'int') {     //讀取數值資料
-        int intData;
-        LoRa.readBytes((uint8_t *) &intData, sizeof(intData));
-        Serial.println(intData);
-        data_out->intData = intData;            //將資料存入data_out
-        return;
-    }
-    else if (type == 'float') {
-        float floatData;
-        LoRa.readBytes((uint8_t *) &floatData, sizeof(floatData));
-        Serial.println(floatData);
-        data_out->floatData = floatData;        //將資料存入data_out
-        return;
-    }
+void Lora_function::Lora_getConfig() {
+    ResponseStructContainer c;         //宣告一個回應結構體
+    c = e220_obj.getConfiguration();   //取得設定
+    Configuration conf = *(Configuration *)c.data;   //取得設定資料
+    Serial.println(c.status.getResponseDescription());   //印出回應描述
+    Serial.println(c.status.code);   //印出回應代碼
+    printParameters(conf);   //印出設定資料
+
+    ResponseStructContainer cMi;      //宣告一個回應結構體
+	cMi = e220_obj.getModuleInformation();
+	ModuleInformation mi = *(ModuleInformation*)cMi.data;
+	Serial.println(cMi.status.getResponseDescription());
+	Serial.println(cMi.status.code);
+	printModuleInformation(mi);
+
+    c.close();  //關閉回應結構體
+    cMi.close();  //關閉回應結構體
+}
+
+void Lora_function::printParameters(struct Configuration configuration) {
+	Serial.println("----------------------------------------");
+
+	Serial.print(F("HEAD : "));  Serial.print(configuration.COMMAND, HEX);Serial.print(" ");Serial.print(configuration.STARTING_ADDRESS, HEX);Serial.print(" ");Serial.println(configuration.LENGHT, HEX);
+	Serial.println(F(" "));
+	Serial.print(F("AddH : "));  Serial.println(configuration.ADDH, HEX);
+	Serial.print(F("AddL : "));  Serial.println(configuration.ADDL, HEX);
+	Serial.println(F(" "));
+	Serial.print(F("Chan : "));  Serial.print(configuration.CHAN, DEC); Serial.print(" -> "); Serial.println(configuration.getChannelDescription());
+	Serial.println(F(" "));
+	Serial.print(F("SpeedParityBit     : "));  Serial.print(configuration.SPED.uartParity, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getUARTParityDescription());
+	Serial.print(F("SpeedUARTDatte     : "));  Serial.print(configuration.SPED.uartBaudRate, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getUARTBaudRateDescription());
+	Serial.print(F("SpeedAirDataRate   : "));  Serial.print(configuration.SPED.airDataRate, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getAirDataRateDescription());
+	Serial.println(F(" "));
+	Serial.print(F("OptionSubPacketSett: "));  Serial.print(configuration.OPTION.subPacketSetting, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getSubPacketSetting());
+	Serial.print(F("OptionTranPower    : "));  Serial.print(configuration.OPTION.transmissionPower, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getTransmissionPowerDescription());
+	Serial.print(F("OptionRSSIAmbientNo: "));  Serial.print(configuration.OPTION.RSSIAmbientNoise, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getRSSIAmbientNoiseEnable());
+	Serial.println(F(" "));
+	Serial.print(F("TransModeWORPeriod : "));  Serial.print(configuration.TRANSMISSION_MODE.WORPeriod, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getWORPeriodByParamsDescription());
+	Serial.print(F("TransModeEnableLBT : "));  Serial.print(configuration.TRANSMISSION_MODE.enableLBT, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getLBTEnableByteDescription());
+	Serial.print(F("TransModeEnableRSSI: "));  Serial.print(configuration.TRANSMISSION_MODE.enableRSSI, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getRSSIEnableByteDescription());
+	Serial.print(F("TransModeFixedTrans: "));  Serial.print(configuration.TRANSMISSION_MODE.fixedTransmission, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getFixedTransmissionDescription());
+
+
+	Serial.println("----------------------------------------");
+}
+
+void Lora_function::printModuleInformation(struct ModuleInformation moduleInformation) {
+	Serial.println("----------------------------------------");
+	Serial.print(F("HEAD: "));  Serial.print(moduleInformation.COMMAND, HEX);Serial.print(" ");Serial.print(moduleInformation.STARTING_ADDRESS, HEX);Serial.print(" ");Serial.println(moduleInformation.LENGHT, DEC);
+
+	Serial.print(F("Model no.: "));  Serial.println(moduleInformation.model, HEX);
+	Serial.print(F("Version  : "));  Serial.println(moduleInformation.version, HEX);
+	Serial.print(F("Features : "));  Serial.println(moduleInformation.features, HEX);
+	Serial.println("----------------------------------------");
+}
+
+void Lora_function::Lora_sendData(struct send_data &data) {
     
-    else if (type == 'String') {
-        String strdata = LoRa.readString();     //讀取字串資料
-        Serial.println(strdata);
-        int splitIndex = strdata.indexOf(':');
-        if (splitIndex != -1) {
-            String get_ID = strdata.substring(0, splitIndex);
-            String data = strdata.substring(splitIndex + 1);
-            if (get_ID == deviceID || get_ID == "ALL") {
-                strdata = data;
-            }
-        }
-        else {
-            Serial.println("Data error.");
-        }
-        data_out->stringData = strdata;         //將資料存入data_out
-        return;
-    }
-    else if (type == 'json') {
-        String jsondata = LoRa.readString();    //讀取json資料
-        Serial.println("response: ");
-        Serial.println(jsondata);
-        StaticJsonDocument<256> doc;
-        deserializeJson(doc, jsondata);  //解析json資料
-        String get_ID = doc["sensor"];
-        return;
-    }
-    else {
-        Serial.println("Type error.");  //類型錯誤
-    }
 }
+
