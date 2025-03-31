c9b55697503c96d39234bea296f9df2ecf94c947
BookCultivator <122209979+BookCultivator@users.noreply.github.com>
2025-03-31 10:36:45 +0800

    delete vens

--- Server_base/apps/mainprogram/app.py
+++ Server_base/apps/mainprogram/app.py
@@ -1,37 +1,34 @@
 from flask import Flask, render_template, jsonify, url_for, request
 import random
 
-app = Flask(__name__)
+
+
+app = Flask(__name__) # 設定靜態檔案和模板檔案的路徑
 
 @app.route("/", methods = ["GET"]) # 設定路由
-def test():
+def index_handle():
     return "Hello. This page is nothing."
 
 @app.route("/interface/web_extremity", methods = ["GET"])
-def index():
+def Web_Extremity_handle():
     return "Hi"
 
 @app.route("/interface/phone_extremity", methods = ["GET"])
-def index():
+def Phone_Extremity_handle():
     return "Hi"
 
 @app.route("/bot/line_bot", methods = ["GET"])
-def index():
+def Line_bot_handle():
     return "Hi"
 
 @app.route("/bot/discord_bot", methods = ["GET"])
-def index():
+def Discord_bot_handle():
     return "hi"
 
 @app.route("/data/sensor_data", methods = ["POST"])
-def index():
+def Sensor_Data_handle():
     return "Hi"
 
-
-
-@app.route("/test2/<name>", methods=["GET"], endpoint="prinnNameEndpoint")
-def test2(name):
-    return f"<h1> {name} </h1>"
 
 # # 模擬獲取即時溫度
 # def get_temperature():
@@ -46,14 +43,6 @@ def test2(name):
 #     # 返回模擬的溫度數據
 #     return jsonify({'temperature': get_temperature()})
 
-@app.route("/test4/<name>", methods=["GET"])
-def test4(name):
-    return render_template("index.html", name=name)
-
-# while app.test_request_context():
-#     print(url_for('test2', name='test2'))
-#     print(url_for('test4', name='dd', page=1))
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
